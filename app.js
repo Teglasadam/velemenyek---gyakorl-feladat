87ea1864c960236526ef58b9367d299fd67284a7
Téglás Ádám <ad.teglas@gmail.com>
2024-01-04 20:10:13 +0100

    a next kesz a back és a css mégh hatra van

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 const velemenyek = [
 	{
-		id: 0,
 		nev: "Nagy Zsanett",
 		munkakor: "Marketing Manager",
 		velemeny:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem rerum quasi sequi ea deleniti cumque vero sint alias, quo eveniet pariatur laborum magni impedit dolorum? Earum illo corporis fuga.",
 	},
 	{
-		id: 1,
 		nev: "Szent Endre",
 		munkakor: "PHP Fejlesztő",
 		velemeny:
 			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem rerum quasi sequi ea deleniti cumque vero sint alias, quo eveniet pariatur laborum magni impedit dolorum? Earum illo corporis fuga.",
 	},
 	{
-		id: 2,
 		nev: "Török Bálint",
 		munkakor: "Automata Tesztelő",
 		velemeny:
@@ -23,16 +20,13 @@ const velemenyek = [
 ];
 
 const velemenydoboz = document.getElementById("velemenydoboz");
-
-let id = 0;
-
-
+const back = document.querySelector(".back");
+const next = document.querySelector(".next");
 
 let felhasznalok = () => {
 	let output = "";
 
 	output += `<div class="order">
-        <p><strong>${velemenyek[id].id}</strong></p>
         <p><strong>${velemenyek[id].nev}</strong></p>
         <p><strong>${velemenyek[id].munkakor}</strong></p>
         <p><strong>${velemenyek[id].velemeny}</strong></p>
@@ -41,4 +35,14 @@ let felhasznalok = () => {
 	velemenydoboz.innerHTML = output;
 };
 
-felhasznalok();
+let id = 0;
+
+next.addEventListener("click", function () {
+	if (id < velemenyek.length-1) {
+		id++;
+		felhasznalok();
+	} else {
+		id = 0;
+		felhasznalok();
+	}
+});
