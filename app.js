2efae689def6a8514cce3124d3876c6dc3ca6474
Teglas Adam <ad.teglas@gmail.com>
2024-01-05 07:34:13 +0100

    Javascript megirva, html markup keszen van, css hianyzik

--- app.js
+++ app.js
@@ -1,48 +1,58 @@
 const velemenyek = [
-	{
-		nev: "Nagy Zsanett",
-		munkakor: "Marketing Manager",
-		velemeny:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem rerum quasi sequi ea deleniti cumque vero sint alias, quo eveniet pariatur laborum magni impedit dolorum? Earum illo corporis fuga.",
-	},
-	{
-		nev: "Szent Endre",
-		munkakor: "PHP Fejlesztő",
-		velemeny:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem rerum quasi sequi ea deleniti cumque vero sint alias, quo eveniet pariatur laborum magni impedit dolorum? Earum illo corporis fuga.",
-	},
-	{
-		nev: "Török Bálint",
-		munkakor: "Automata Tesztelő",
-		velemeny:
-			"Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem rerum quasi sequi ea deleniti cumque vero sint alias, quo eveniet pariatur laborum magni impedit dolorum? Earum illo corporis fuga.",
-	},
+  {
+    nev: "Nagy Zsanett",
+    munkakor: "Marketing Manager",
+    velemeny:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem rerum quasi sequi ea deleniti cumque vero sint alias, quo eveniet pariatur laborum magni impedit dolorum? Earum illo corporis fuga.",
+  },
+  {
+    nev: "Szent Endre",
+    munkakor: "PHP Fejlesztő",
+    velemeny:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem rerum quasi sequi ea deleniti cumque vero sint alias, quo eveniet pariatur laborum magni impedit dolorum? Earum illo corporis fuga.",
+  },
+  {
+    nev: "Török Bálint",
+    munkakor: "Automata Tesztelő",
+    velemeny:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptatem rerum quasi sequi ea deleniti cumque vero sint alias, quo eveniet pariatur laborum magni impedit dolorum? Earum illo corporis fuga.",
+  },
 ];
 
 const velemenydoboz = document.getElementById("velemenydoboz");
 const back = document.querySelector(".back");
 const next = document.querySelector(".next");
+let id = 0;
 
-let felhasznalok = () => {
-	let output = "";
+let felhasznalo = () => {
+  let output = "";
 
-	output += `<div class="order">
+  output += `<div class="order">
         <p><strong>${velemenyek[id].nev}</strong></p>
         <p><strong>${velemenyek[id].munkakor}</strong></p>
         <p><strong>${velemenyek[id].velemeny}</strong></p>
         </div>`;
 
-	velemenydoboz.innerHTML = output;
+  velemenydoboz.innerHTML = output;
 };
 
-let id = 0;
+window.addEventListener("load", felhasznalo(0));
 
 next.addEventListener("click", function () {
-	if (id < velemenyek.length-1) {
-		id++;
-		felhasznalok();
-	} else {
-		id = 0;
-		felhasznalok();
-	}
+  if (id < velemenyek.length - 1) {
+    id++;
+    felhasznalo();
+  } else {
+    id = 0;
+    felhasznalo();
+  }
+});
+back.addEventListener("click", function () {
+  if (id > 0) {
+    id--;
+    felhasznalo();
+  } else {
+    id = velemenyek.length - 1;
+    felhasznalo();
+  }
 });
